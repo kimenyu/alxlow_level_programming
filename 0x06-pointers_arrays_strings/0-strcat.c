7c0b21b86351f735f8f2040e318dd6a31a0f6ea3
kimenyu <njorogekimenyu@gmail.com>
2022-12-22 08:03:25 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,10 +5,22 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-int main(void)
+char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
-	strcat(int a, int b);
-	return 0;
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (b >= 0)
+	{
+		dest[a] = src[b];
+		if (src[b] == '\0')
+			break;
+		a++;
+		b++;
+	}
+	return (dest);
 }
