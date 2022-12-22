0f33087272f8fc5893ef53789e442012969f0ed3
kimenyu <njorogekimenyu@gmail.com>
2022-12-22 07:57:41 +0300

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+int main(void)
+{
+	int a;
+	int b;
+	strcat(int a, int b);
+	return 0;
+}
